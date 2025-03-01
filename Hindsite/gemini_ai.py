0fa8c00198247fe14d25a1550a39b61a8ac04e52
Timothy Sturges <tos3454@rit.edu>
2025-03-01 16:22:20 -0500

    Made a rough draft of a function that gets public perception and technical impact based on headlines

--- Hindsite/gemini_ai.py
+++ Hindsite/gemini_ai.py
@@ -31,3 +31,34 @@ def generate_hints(stock_data):
         hints.append("GEMINI AI RESPONSE FAILURE")
 
     return hints
+
+def generate_trend(headline):
+    public_perception = 0, technical_impact = 0
+    prompt = f"""
+    Given the following headlines:
+    - HEADLINE1: ${data["HEADLINE1"]}
+    - HEADLINE2: ${data["HEADLINE2"]}
+    - HEADLINE3: ${data["HEADLINE3"]}
+
+    Based on these headlines and the way they describe the company's changes and possible problems, 
+    generate two constants that describe the public's perception of the company(1-20, 1 being most negative, 20 being most positive)
+    and the actual impact the news described in the headline may actually have on the functionality of the company
+    from a technical standpoint (1-20, 1 being most negative, 20 being most positive). Please just only give the two numbers, nothing else
+    as a response, with the public perception first and the technical impact second.
+    """
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash', 
+            contents=prompt
+        )
+        constants = response.text
+        for c in constants:
+            if c.isdigit():
+                if public_perception == 0:
+                    public_perception = int(c)
+                else:
+                    technical_impact = int(c)
+        return public_perception, technical_impact
+
+    except Exception as e:
+        return "GEMINI AI RESPONSE FAILURE"
