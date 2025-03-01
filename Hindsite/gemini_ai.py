7a36906617665f950e6fb61b52aa469eb3079b0a
Timothy Sturges <tos3454@rit.edu>
2025-03-01 16:48:01 -0500

    Changed headlines to be randomly generated and then interpretted by gemini

--- Hindsite/gemini_ai.py
+++ Hindsite/gemini_ai.py
@@ -13,12 +13,21 @@ def generate_hints(stock_data):
     hints = []
 
     prompt = f"""
-    Given the following stock price changes:
+    Given the following companies and their stock prices:
     - COMPANY1: ${data["COMPANY1"]}
     - COMPANY2: ${data["COMPANY2"]}
     - COMPANY3: ${data["COMPANY3"]}
+    - COMPANY4: ${data["COMPANY4"]}
+    - COMPANY5: ${data["COMPANY5"]}
+    - COMPANY6: ${data["COMPANY6"]}
+    - COMPANY7: ${data["COMPANY7"]}
+    - COMPANY8: ${data["COMPANY8"]}
+    - COMPANY9: ${data["COMPANY9"]}
+    - COMPANY10: ${data["COMPANY10"]}
 
-    Generate short news headlines that hint at reasons for these changes.
+    Generate short news headlines that may be randomly negative or positive on changes within the company.
+    Usually either will be focused on staff misconduct, or new innovations, or any other form of common headline
+    that may be made about a company.
     """
 
     try:
@@ -39,6 +48,13 @@ def generate_trend(headline):
     - HEADLINE1: ${data["HEADLINE1"]}
     - HEADLINE2: ${data["HEADLINE2"]}
     - HEADLINE3: ${data["HEADLINE3"]}
+    - HEADLINE4: ${data["HEADLINE4"]}
+    - HEADLINE5: ${data["HEADLINE5"]}
+    - HEADLINE6: ${data["HEADLINE6"]}
+    - HEADLINE7: ${data["HEADLINE7"]}
+    - HEADLINE8: ${data["HEADLINE8"]}
+    - HEADLINE9: ${data["HEADLINE9"]}
+    - HEADLINE10: ${data["HEADLINE10"]}
 
     Based on these headlines and the way they describe the company's changes and possible problems, 
     generate two constants that describe the public's perception of the company(1-20, 1 being most negative, 20 being most positive)
