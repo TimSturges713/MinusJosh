7f5f116e25d6b952c1d12e30898e2d627321edfd
Timothy Sturges <tos3454@rit.edu>
2025-03-01 22:41:47 -0500

    Fix:

--- Hindsite/stock_data.py
+++ Hindsite/stock_data.py
@@ -1,8 +1,4 @@
 import random
-
-def get_stock_trends(technical_change, public_perception):
-    # the time period between the turns in weeks
-    period = random.randint(5, 10)
     
 def get_stock_trends(company_price, public_perception, technical_impact):
     # the time period between the turns in weeks
