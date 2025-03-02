26f09358870eedfff98b10e3e508be10257cf174
Timothy Sturges <tos3454@rit.edu>
2025-03-01 20:06:53 -0500

    Makes the start company dictionary

--- Hindsite/gemini_ai.py
+++ Hindsite/gemini_ai.py
@@ -119,3 +119,26 @@ def generate_trend(data):
 
     except Exception as e:
         return "GEMINI AI RESPONSE FAILURE"
+    
+
+def game_start_gen():
+    stocks = dict()
+    prompt = f"""
+    Retrieve ten random well-known companies (with one word names) on the stock market and their respective costs.
+    Randomly offset these companies stock prices by about 1-10% of their current value, raising it or lowering it.
+    Then list the company's names and their offset stock prices. List them with nothing else other than separating company
+    name and stock value by a comma. Each new line is a different company and each line ends with a comma.
+    """
+    try:
+        response.client.models.generate_content(
+            model='gemini-2.0-flash', 
+            contents=prompt
+        )
+        stock = response.text
+        stock = stock.split(",")
+        flag = 0
+        for c in range(0, stocks.length(), 2):
+            stocks[stock[c]] = stock[c+1]
+        return stocks
+    except Exception as e:
+        return "GEMINI AI RESPONSE FAILURE"
