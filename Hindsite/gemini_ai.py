4726dab7c7a95598ef91cd664b5f0ad4d6131b1a
Timothy Sturges <tos3454@rit.edu>
2025-03-01 19:19:29 -0500

    Added more gemini functionality

--- Hindsite/gemini_ai.py
+++ Hindsite/gemini_ai.py
@@ -13,34 +13,29 @@ class Headline(BaseModel):
     title: str
     detail: str
 
+class Comment(BaseModel):
+    comment: str
+    likes: int
+
 
 
 # Generate AI headlines for company stories
 def generate_headlines(data):
-    headlines = []
+    headline = ""
 
     prompt = f"""
-    Given the following companies and their previous headlines (if any):
+    Given the company and their previous headlines (if any):
     - COMPANY1: ${data["COMPANY1"]}
-    - COMPANY2: ${data["COMPANY2"]}
-    - COMPANY3: ${data["COMPANY3"]}
-    - COMPANY4: ${data["COMPANY4"]}
-    - COMPANY5: ${data["COMPANY5"]}
-    - COMPANY6: ${data["COMPANY6"]}
-    - COMPANY7: ${data["COMPANY7"]}
-    - COMPANY8: ${data["COMPANY8"]}
-    - COMPANY9: ${data["COMPANY9"]}
-    - COMPANY10: ${data["COMPANY10"]}
 
-    Generate short news headlines that may be randomly negative or positive on changes within the company.
+    Generate a short news headline that may be randomly negative or positive on changes within the company.
     Usually either will be focused on staff misconduct, or new innovations, or any other form of common headline
-    that may be made about a company. Each company may have previous headlines, if so make the new headline based off
-    previous ones, companies can learn from their previous mistakes or go further into failure, you decide. Then give 
-    a list of all the new headlines for each respective company. There's only one headline 
-    per company. Start each headline with a '*' character, and 
-    end it with a ';' character to show where it begins and ends. The headline will the title, and any added details
+    that may be made about a company. The company may have previous headlines, if so make the new headline based off
+    previous ones, the company can learn from their previous mistakes or go further into failure, you decide. Then give 
+    the new headline for the company. There's only one new headline generated
+    for the company. Start the headline with a '*' character, and 
+    end it with a ';' character to show where it begins and ends. The headline will be the title, and any added details
     will be the details. Put the headline in the title part of the JSON object, and the details in the details
-    part of the JSON object. Return those in a list of JSON objects for each company.
+    part of the JSON object. Return this in a JSON object.
     """
 
     try:
@@ -52,31 +47,54 @@ def generate_headlines(data):
             'response_schema': Headline,
             },
         )
-        return response.parsed
+        headline = response.parsed
 
     except Exception as e:
-        headlines.append("GEMINI AI RESPONSE FAILURE")
+        headline = "GEMINI AI RESPONSE FAILURE"
+    (public_perception, technical_impact) = generate_trend(headline)
+
+def generate_comment(headline, public_perception):
+    prompt = f"""
+    Given the headline for a company's recent news report and the public's perception of the company's changes,
+    (Note: the public perception is an integer from 1-20, 1 being most negative, 20 vice versa):
+    - HEADLINE: ${headline}
+    - PUBLIC_PERCEPTION: ${public_perception}
+
+    Write 3-5 comments that may be made from viewers online looking at the headline's story.
+    These comments will align with the public's perception and accurately describe how they feel.
+    Depending on how aligned these comments are with the public's views, the more likes they'll have.
+    Format this answer into a JSON object with comment and likes as the two attributes in the object.
+    """
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash', 
+            contents=prompt,
+        config={
+            'response_mime_type': 'application/json',
+            'response_schema': Comment,
+            },
+        )
+        comment = response.parsed
+    except Exception as e:
+        comment = "GEMINI AI RESPONSE FAILURE"
+    return comment
+
+
     
+
+
 def generate_trend(data):
     public_perception = 0, technical_impact = 0
     prompt = f"""
-    Given the following headlines:
-    - HEADLINE1: ${data["HEADLINE1"]}
-    - HEADLINE2: ${data["HEADLINE2"]}
-    - HEADLINE3: ${data["HEADLINE3"]}
-    - HEADLINE4: ${data["HEADLINE4"]}
-    - HEADLINE5: ${data["HEADLINE5"]}
-    - HEADLINE6: ${data["HEADLINE6"]}
-    - HEADLINE7: ${data["HEADLINE7"]}
-    - HEADLINE8: ${data["HEADLINE8"]}
-    - HEADLINE9: ${data["HEADLINE9"]}
-    - HEADLINE10: ${data["HEADLINE10"]}
+    Given the following headline JSON object:
+    - HEADLINE1: ${data}
 
-    Based on these headlines and the way they describe the company's changes and possible problems, 
+    Based on the headline and the way it describes the company's changes and possible problems, 
     generate two constants that describe the public's perception of the company(1-20, 1 being most negative, 20 being most positive)
     and the actual impact the news described in the headline may actually have on the functionality of the company
     from a technical standpoint (1-20, 1 being most negative, 20 being most positive). Please just only give the two numbers, nothing else
-    as a response, with the public perception first and the technical impact second.
+    as a response, with the public perception first and the technical impact second. Formatted like i,j just being separated
+    by a comma.
     """
     try:
         response = client.models.generate_content(
@@ -84,6 +102,7 @@ def generate_trend(data):
             contents=prompt
         )
         constants = response.text
+        constants = constants.split(",")
         for c in constants:
             if c.isdigit():
                 if public_perception == 0:
